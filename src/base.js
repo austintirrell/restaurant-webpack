a964e1644055b9beb70a990173d44147978b7783
Austin Tirrell <101142641+austintirrell@users.noreply.github.com>
2023-01-22 22:33:04 -0600

    base template for site

---
+++ src/base.js
@@ -0,0 +1,18 @@
+import { createElement } from "./DOM"
+
+function renderBase() {
+  const content = document.getElementById('content')
+
+  const header = createElement('header', null, null, content)
+
+  createElement('h1', [['className', 'company-name']], 'Pizza Stop', header)
+
+  const tabsContainer = createElement('div', [['className', 'tabs-container']], null, header)
+  createElement('button', [['className', 'tab-button'], ['id', 'home-button']], 'Home', tabsContainer)
+  createElement('button', [['className', 'tab-button'], ['id', 'menu-button']], 'Menu', tabsContainer)
+  createElement('button', [['className', 'tab-button'], ['id', 'contact-button']], 'Contact', tabsContainer)
+
+  createElement('div', [['id', 'tab-content']], null, content)
+}
+
+export { renderBase }
