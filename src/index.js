a94cab9faea5d546a2c20e16bc46d3117c4ab684
Austin Tirrell <101142641+austintirrell@users.noreply.github.com>
2023-01-22 23:59:07 -0600

    logic for webpage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import { renderBase } from './base'
 import { renderHomepage } from './homepage'
+import { renderMenu } from './menu'
+import { renderContact } from './contact'
 
-window.onload = () => {
+window.onload = () => init()
+
+function init() {
   renderBase()
   renderHomepage()
+
+  const homeButton = document.getElementById('home-button')
+  homeButton.onclick = () => renderHomepage()
+
+  const menuButton = document.getElementById('menu-button')
+  menuButton.onclick = () => renderMenu()
+
+  const contactButton = document.getElementById('contact-button')
+  contactButton.onclick = () => renderContact()
 }
